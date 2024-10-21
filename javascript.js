d900157abb8d8b3e375b546735ec2653677268ee
Alexander Garyaev <125agaryaev@frhsd.com>
2024-10-21 13:16:47 -0400

    working on it

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function addCart1()
+{
+    window.location.href = "Burger";
+}
+function addCart2()
+{
+    window.location.href = "HotDog";
+}
+function addCart3()
+{
+    window.location.href = "Pizza";
+}
+function addCart4()
+{
+    window.location.href = "Cart";
+}
+function addCart5()
+{
+    window.location.href = "Cart";
+}
+function addCart6()
+{
+    window.location.href = "Cart";
+}
