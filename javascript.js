e51589393d2a06785d94aeced2c5a35b717ce44f
Alexander Garyaev <125agaryaev@frhsd.com>
2024-10-29 12:54:25 -0400

    working on it

--- javascript.js
+++ javascript.js
@@ -1,52 +1,81 @@
 let items = [];
-function addCart1()
+function tpBurger()
 {
     window.location.href = "Burger";
 }
-function addCart2()
+function tpHotdog()
 {
     window.location.href = "HotDog";
 }
-function addCart3()
+function tpPizza()
 {
     window.location.href = "Pizza";
 }
-function addCart4()
-{
-    window.location.href = "../Cart";
-}
-function addCart5()
-{
-    window.location.href = "../Cart";
-}
-function addCart6()
-{
-    window.location.href = "../Cart";
-}
-function addCart()
-{
-    if(localStorage.getItem("wiz"))
-    {
-        items.push(JSON.parse(localStorage.getItem("wiz")));
-    }
-    var saving =
-        {
-            name: "burger", price: 5
-        }
-    items.push(saving);
-    localStorage.setItem("wiz", JSON.stringify(items));
-    load();
-}
 function load()
 {
-    if(localStorage.getItem("wiz"))
+    if (localStorage.getItem("wiz"))
     {
         items = JSON.parse(localStorage.getItem("wiz"));
     }
+}
+function addCart(x, y)
+{
+    var saving =
+        {
+            name: x, price: y
+        }
+    items.push(saving);
+    localStorage.setItem("wiz", JSON.stringify(items));
+}
+function addCart2(x, y, z)
+{
+    var saving =
+        {
+            name: x, price: y, toppings: z
+        }
+    items.push(saving);
+    localStorage.setItem("wiz", JSON.stringify(items));
+}
+function loadCart()
+{
+    load()
     let x = "";
     for (let y = items.length - 1; y >= 0; y--)
     {
         x += "<p>" + items[y].price + "</p>"
     }
     document.getElementById("wiz").innerHTML = x;
+}
+function burgerSort()
+{
+    let arr = ["With lettuce and onions", "No lettuce", "No onions"]
+    for (let i = arr.length - 1; i >=0; i--)
+    {
+        if (document.getElementById(arr[i]).checked)
+        {
+            return arr[i];
+        }
+    }
+}
+function hotdogSort()
+{
+    let arr = ["Normal (Mustard)", "Just hot dog", "ketchup and mustard"]
+    for (let i = arr.length - 1; i >=0; i--)
+    {
+        if (document.getElementById(arr[i]).checked)
+        {
+            return arr[i];
+        }
+    }
+}
+function pizzaSort()
+{
+    let arr = ["Normal (pepperoni)", "Plain", "margarita"]
+    for (let i = arr.length - 1; i >=0; i--)
+    {
+        if (document.getElementById(arr[i]).checked)
+        {
+            return arr[i];
+        }
+    }
 }
